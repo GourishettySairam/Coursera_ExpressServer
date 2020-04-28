2d9c13d177c2f895c1797367e0f15587b01d4872
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2020-04-28 23:12:58 +0530

    Assignment 3

--- routes/users.js
+++ routes/users.js
@@ -9,9 +9,18 @@ var router = express.Router();
 router.use(bodyParser.json());
 
 
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) =>
+{
+    //console.log(req);
+      User.find({})
+        .then((users) => {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json(users);
+      }, (err) => next(err))
+      .catch((err) => next(err));
+  })
+
 
 
 router.post('/signup', (req, res, next) => {
@@ -45,9 +54,9 @@ router.post('/signup', (req, res, next) => {
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
-  console.log("request is ",req)
+//  console.log("request is ",req)
   var token = authenticate.getToken({_id: req.user._id});
-  console.log("token is ",token)
+//  console.log("token is ",token)
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.json({success: true, token: token, status: 'You are successfully logged in!'});
@@ -65,5 +74,6 @@ router.get('/logout', (req, res,next) => {
     next(err);
   }
 });
+
 
 module.exports = router ;

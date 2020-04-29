3cbb12d3c783d13ee4c46a550920529c514f7e67
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2020-04-29 15:59:13 +0530

    Cross Origin Resource Sharing ( CORS )

--- routes/users.js
+++ routes/users.js
@@ -4,12 +4,13 @@ var User = require('../models/user');
 var passport = require('passport');
 var authenticate = require('../authenticate');
 var config = require('../config');
+const cors = require('./cors');
 
 var router = express.Router();
 router.use(bodyParser.json());
 
 
-router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) =>
+router.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) =>
 {
     //console.log(req);
       User.find({})
@@ -23,7 +24,7 @@ router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, (req, res, ne
 
 
 
-router.post('/signup', (req, res, next) => {
+router.post('/signup',cors.corsWithOptions, (req, res, next) => {
   User.register(new User({username: req.body.username}),
     req.body.password, (err, user) => {
     if(err) {
@@ -53,7 +54,7 @@ router.post('/signup', (req, res, next) => {
   });
 });
 
-router.post('/login', passport.authenticate('local'), (req, res) => {
+router.post('/login',cors.corsWithOptions, passport.authenticate('local'), (req, res) => {
 //  console.log("request is ",req)
   var token = authenticate.getToken({_id: req.user._id});
 //  console.log("token is ",token)
@@ -62,7 +63,7 @@ router.post('/login', passport.authenticate('local'), (req, res) => {
   res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
-router.get('/logout', (req, res,next) => {
+router.get('/logout', cors.corsWithOptions, (req, res,next) => {
   if (req.session) {
     req.session.destroy();
     res.clearCookie('session-id');
